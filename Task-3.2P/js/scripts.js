9943405b651d79d20136ff1f74042b02394bc485
TitikshaDeakin <s225090611@deakin.edu.au>
2025-08-01 09:54:25 +1000

    Added Task-3.2P with Materialize enhancements

---
+++ Task-3.2P/js/scripts.js
@@ -0,0 +1,26 @@
+function changeText() {
+    var textsArray = ["Text 1", "Text 2", "Text 3", "Text 4", "Text 5"]
+    var number = getRandomNumberBetween(0, textsArray.length - 1)
+    console.log("Index: ", number)
+    document.getElementById("heading").innerHTML = textsArray[number];
+}
+function getRandomNumberBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const firstName = document.getElementById('first_name').value;
+    const lastName = document.getElementById('last_name').value;
+    const email = document.getElementById('email').value;
+
+    document.getElementById('output').innerText =
+      `Name: ${firstName} ${lastName}\nEmail: ${email}`;
+    
+    const resultCard = document.getElementById('formResult');
+    resultCard.style.display = 'block';
+    resultCard.classList.add('fade-in'); // apply animation
+  });
+});
+
